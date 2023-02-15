f9379961434d3d3c36127c2e3faeaeb0da1e5f80
kewhudson <kewwoof@icloud.com>
2023-02-15 18:45:48 +0100

    Creata tabella con dati da API

--- src/components/VillagerTable/VillagerTable.js
+++ src/components/VillagerTable/VillagerTable.js
@@ -1,11 +1,53 @@
 import React from "react";
-
-
+import {NavLink} from "react-router-dom";
 
 function VillagerTable(props) {
     const {VillagerList} = props;
 
-    return "VillagerTable"
+    let villagerTable = [];
+    for(let villager = 0; villager < 391; villager ++){
+        villagerTable.push(
+            <tr key={villager+1}>
+
+                    <td>
+                        <NavLink to={`/RegistryOffice/${villager+1}`}>
+                            {Object.values(VillagerList)[villager]['id']}
+                        </NavLink>
+                    </td>
+                    <td>
+                        <NavLink to={`/RegistryOffice/${villager+1}`}>
+                            {Object.values(VillagerList)[villager]['name']['name-EUit']}
+                        </NavLink>
+                    </td>
+                    <td>
+                        <NavLink to={`/RegistryOffice/${villager+1}`}>
+                            <img src={Object.values(VillagerList)[villager]['icon_uri']}/>
+                        </NavLink>
+                    </td>
+                    <td>
+                        <NavLink to={`/RegistryOffice/${villager+1}`}>
+                            {Object.values(VillagerList)[villager]['species']}
+                        </NavLink>
+                    </td>
+            </tr>
+        );
+    };
+
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Immagine</th>
+                <th>Specie</th>
+            </tr>
+            </thead>
+            <tbody>
+            {villagerTable}
+            </tbody>
+        </table>
+    );
 }
 
 export default VillagerTable;
