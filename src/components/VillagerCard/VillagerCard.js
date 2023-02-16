327801ce01a18f4742a016b045fd32153118f3f0
Martina Bisi <m.bisi2@campus.unimib.it>
2023-02-16 18:24:11 +0100

    CardsGrid da modificare

--- src/components/VillagerCard/VillagerCard.js
+++ src/components/VillagerCard/VillagerCard.js
@@ -1,7 +1,24 @@
 import React from "react";
+import {Card, CardBody, CardImg, CardText, CardTitle} from "reactstrap";
+import style from "/VillagerCard.module.css";
+import {zeroPad} from "../../Utility/utility";
 
-function VillagerCard() {
-    return "VillagerCard"
+
+/*
+function VillagerCard(props) {
+    const {nome, ID, immagine, specie} = props;
+
+    return(
+        <Card className={style.card}>
+            <CardImg className={style.immagine} top width="100%" src={immagine} alt={nome}/>
+            <CardBody className="text-center">
+                <CardTitle tag="h5" className={'h3 ${style.title}'}> {nome}</CardTitle>
+                <CardText>
+                    #{zeroPad(ID, 3)}
+                </CardText>
+            </CardBody>
+        </Card>
+    )
 }
-
+ IDEM PANIKKKK */
 export default VillagerCard;
--- src/components/VillagerCardsGrid/VillagerCardsGrid.js
+++ src/components/VillagerCardsGrid/VillagerCardsGrid.js
@@ -1,7 +1,31 @@
 import React from "react";
+import VillagerCard from "../VillagerCard/VillagerCard";
 
-function VillagerCardsGrid() {
-    return "VillagerCardsGrid"
+/*
+function VillagerCardsGrid(props) {
+    const {VillagerList, col} = props;
+
+    const VillagerCardsCol = VillagerList.map ((villager) => {
+        return (
+        <div key={villager.id} className="col">
+            <VillagerCard
+            nome={Object.values(VillagerList)[villager]['name']['name-EUit']}
+
+            ID={Object.values(VillagerList)[villager]['id']}
+            immagine={Object.values(VillagerList)[villager]['icon_uri']}
+            specie={Object.values(VillagerList)[villager]['species']}
+            />
+        </div>
+        )
+    });
+
+    return(
+        <div className={'row row-cols-${col.xs} row-cols-sm-${col.sm} row-cols-md-${col.md} row-cols-lg-${col.lg} row-cols-xl-${col.xl}'}>
+        {VillagerCardsCol}
+            </div>
+    )
 }
+
+IO CHE SCRIVO COSE NEL PANIK */
 
 export default VillagerCardsGrid;
