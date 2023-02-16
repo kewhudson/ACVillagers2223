679f43eda19305322a26833774e1dd64e36a05fa
kewhudson <kewwoof@icloud.com>
2023-02-16 19:56:25 +0100

    Valore reso const

--- src/components/VillagerTable/VillagerTable.js
+++ src/components/VillagerTable/VillagerTable.js
@@ -4,34 +4,35 @@ import {NavLink} from "react-router-dom";
 function VillagerTable(props) {
     const {VillagerList} = props;
 
-    let villagerTable = [];
+    const villagerTable = [];
+
     for(let villager = 0; villager < 391; villager ++){
         villagerTable.push(
             <tr key={villager+1}>
 
                     <td>
                         <NavLink to={`/RegistryOffice/${villager+1}`}>
-                            {Object.values(VillagerList)[villager]['id']}
+                            {VillagerList[villager]['id']}
                         </NavLink>
                     </td>
                     <td>
                         <NavLink to={`/RegistryOffice/${villager+1}`}>
-                            {Object.values(VillagerList)[villager]['name']['name-EUit']}
+                            {VillagerList[villager]['name']['name-EUit']}
                         </NavLink>
                     </td>
                     <td>
                         <NavLink to={`/RegistryOffice/${villager+1}`}>
-                            <img src={Object.values(VillagerList)[villager]['icon_uri']}/>
+                            <img src={VillagerList[villager]['icon_uri']}/>
                         </NavLink>
                     </td>
                     <td>
                         <NavLink to={`/RegistryOffice/${villager+1}`}>
-                            {Object.values(VillagerList)[villager]['species']}
+                            {VillagerList[villager]['species']}
                         </NavLink>
                     </td>
             </tr>
         );
-    };
+    }
 
     return (
         <table>
