62c52c0b871fa4bb05a159e4dde23d6567f30664
kewhudson <kewwoof@icloud.com>
2023-02-17 21:48:25 +0100

    Aggiunti 3 villager preferiti in home

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,12 +1,23 @@
 import React from "react";
+import villagerPreferiti from "../../assets/data/villagerBaseData.json"
+import VillagerCardsGrid from "../../components/VillagerCardsGrid/VillagerCardsGrid";
+import {NavLink} from "react-router-dom";
 
 function Home() {
     return (
         <div className="container">
              <div className="row justify-content-center">
                  <div className="col">
-QUESTA È HOME
+                     <h1 className='text-center'>Ti presentiamo i nostri Villager preferiti</h1>
 
+                     <VillagerCardsGrid
+                         VillagerList={Object.values(villagerPreferiti)}
+                         col={{xs: 1, sm: 2, md: 2, lg: 3, xl: 3}}
+                     />
+
+                     <NavLink className="button" to="/RegistryOffice">Guarda l'elenco completo</NavLink>
+
+                     <h1 className='text-center'>Oggi è il compleanno di: </h1>
                  </div>
              </div>
         </div>
