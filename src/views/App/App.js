8404c24fc05ceee306ba4175db8a1cef3d17d705
kewhudson <kewwoof@icloud.com>
2023-02-23 22:47:20 +0100

    Scroll to Top

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MainTemplate from "../../components/MainTemplate/MainTemplate";
 import Home from "../Home/Home";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Info from "../Info/Info";
 import RegistryOffice from "../RegistryOffice/RegistryOffice";
 import VillagerDetail from "../VillagerDetail/VillagerDetail";
 import LogoNavBar from "../../assets/img/Animal_Crossing_Leaf.svg";
+import NotFound from "../NotFound/NotFound";
+import ScrollToTop from "../../ScrollToTop";
 
 function App() {
 
@@ -24,12 +26,15 @@ function App() {
               logo={LogoNavBar}
           >
 
+             <ScrollToTop>
            <Routes>
              <Route path="/" element={<Home />} />
              <Route path="/RegistryOffice" element={<RegistryOffice />} />
              <Route path="/Info" element={<Info />}/>
              <Route path="/RegistryOffice/:number" element={<VillagerDetail />}/>
+             <Route path="*" element={<NotFound />} />
            </Routes>
+             </ScrollToTop>
 
          </MainTemplate>
       </BrowserRouter>
