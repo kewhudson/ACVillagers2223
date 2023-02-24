6f0142340ef58288d87e8d43c9aa2b3f676fe6dd
kewhudson <kewwoof@icloud.com>
2023-02-24 13:45:57 +0100

    Minor fix

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -6,7 +6,7 @@ import {NavLink} from "react-router-dom";
 function NotFound() {
     return (
         <div className='container text-center'>
-            <img src={Resetti} alt="Resetti con in mano un piccone" className={style.img}/>
+            <NavLink to="/"><img src={Resetti} alt="Resetti con in mano un piccone" className={style.img}/></NavLink>
             <h1>Sei finito nella pagina 404</h1>
             <h2>Quante volte ti ho detto di non uscire dalle rotte?!</h2>
             <NavLink className="button mt-3 mb-4" to="/">Torna alla Home</NavLink>
