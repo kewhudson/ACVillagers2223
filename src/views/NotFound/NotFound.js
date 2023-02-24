2494254879562831bcbc3b3b3c7fc4f7d305c5ae
Martina Bisi <m.bisi2@campus.unimib.it>
2023-02-24 15:39:25 +0100

    footer e bottone

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -8,8 +8,8 @@ function NotFound() {
         <div className='container text-center'>
             <NavLink to="/"><img src={Resetti} alt="Resetti con in mano un piccone" className={style.img}/></NavLink>
             <h1>Sei finito nella pagina 404</h1>
-            <h2>Quante volte ti ho detto di non uscire dalle rotte?!</h2>
-            <NavLink className="button mt-3 mb-4" to="/">Torna alla Home</NavLink>
+            <h2 className="mb-5">Quante volte ti ho detto di non uscire dalle rotte?!</h2>
+            <NavLink className="button mt-3 mb-4 fw-bold" to="/">Torna alla Home</NavLink>
         </div>
     )
 }
--- src/views/VillagerDetail/VillagerDetail.js
+++ src/views/VillagerDetail/VillagerDetail.js
@@ -41,8 +41,8 @@ function VillagerDetail() {
     <>
         {(villagerID < 0 || villagerID > 391) ?
                 <div className='text-center'>
-                    <h3 className='mx-2'>Ci sono solo 391 abitanti!</h3>
-                    <NavLink className="button mb-4" to="/RegistryOffice">Torna alla lista degli abitanti</NavLink>
+                    <h3 className='mx-2 mb-5'>Ci sono solo 391 abitanti!</h3>
+                    <NavLink className="button mb-4 fw-bold" to="/RegistryOffice">Torna alla lista degli abitanti</NavLink>
                 </div> :
 
                 <>
