48149d00d28ef46cf1654ffbfc88d8d3242cc9eb
kewhudson <kewwoof@icloud.com>
2023-02-24 15:25:24 +0100

    Limit check

--- src/views/VillagerDetail/VillagerDetail.js
+++ src/views/VillagerDetail/VillagerDetail.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import {NavLink, useParams} from "react-router-dom";
 import style from "./VillagerDetail.module.css";
 import speciesTranslation from "../../functions/SpeciesTranslation";
 import personalitiesTranslation from "../../functions/PersonalitiesTranslation";
@@ -39,61 +39,68 @@ function VillagerDetail() {
     /* FINE API CALL */
 
     return (
-        <div>
+    <>
+        {(villagerID < 0 || villagerID > 391) ?
+                <div className='text-center'>
+                    <h3 className='mx-2'>Ci sono solo 391 abitanti!</h3>
+                    <NavLink className="button mb-4" to="/RegistryOffice">Torna alla lista degli abitanti</NavLink>
+                </div> :
 
-            {villagerData.length !== 0 ?
-                    <div className="container">
-                        <div className="row  align-items-center mt-4">
-                            <div className="col-2 col-xs-0"> </div>
+                <>
+                    {villagerData.length !== 0 ?
+                            <div className="container">
+                                <div className="row  align-items-center mt-4">
+                                    <div className="col-2 col-xs-0"> </div>
 
 
-                            <div className="col-md-3 d-flex justify-content-center">
-                                <div className={style.polaroid} /*style={{background: villagerData["bubble-color"]}}*/>
-                                <img src={villagerData['image_uri']} className={style.imgPolaroid} alt={villagerData['name']}/>
+                                    <div className="col-md-3 d-flex justify-content-center">
+                                        <div className={style.polaroid} /*style={{background: villagerData["bubble-color"]}}*/>
+                                        <img src={villagerData['image_uri']} className={style.imgPolaroid} alt={villagerData['name']}/>
+                                        </div>
+                                    </div>
+
+                                    <div className="col-1 "> </div>
+
+                                    <div className="col-sm-8 col-md-4 d-flex flex-column justify-content-center ps-5 pt-3 pt-sm-0 ms-sm-3 ps-sm-5 ms-md-0 ps-md-0">
+                                        <h2 className="fw-bold "> {villagerData['name']['name-EUit']}</h2>
+
+                                        <ul className={style.caratteristiche}>
+                                            <li className={style.cuore}> <span className='fw-bold'>Specie:</span> {speciesTranslation(villagerData['species'])}</li>
+                                            <li className={style.cuore}> <span className='fw-bold'>Compleanno</span>: {villagerData['birthday']}</li>
+                                            <li className={style.cuore}> <span className='fw-bold'>Genere:</span> {genderTranslation(villagerData['gender'])} </li>
+                                            <li className={style.cuore}> <span className='fw-bold'>Personalità:</span> {personalitiesTranslation(villagerData['personality'])} </li>
+                                        </ul>
+
+                                    </div>
+
+                                    <div className="col-2"> </div>
+
                                 </div>
-                            </div>
 
-                            <div className="col-1 "> </div>
+                                <div className="row mt-3">
+                                    <div className="col d-flex flex-column align-items-center">
+                                        <div className={style.thought}> <p className="text-center"><span className='fw-bold'>Motto:</span> {villagerData['catch-translations']['catch-EUit']}</p>  </div>
+                                        <p  className="text-center mt-4"><span className='fw-bold'>La sua citazione:</span> "{villagerData['saying']}"</p>
+                                    </div>
 
-                            <div className="col-sm-8 col-md-4 d-flex flex-column justify-content-center ps-5 pt-3 pt-sm-0 ms-sm-3 ps-sm-5 ms-md-0 ps-md-0">
-                                <h2 className="fw-bold "> {villagerData['name']['name-EUit']}</h2>
+                                </div>
 
-                                <ul className={style.caratteristiche}>
-                                    <li className={style.cuore}> <span className='fw-bold'>Specie:</span> {speciesTranslation(villagerData['species'])}</li>
-                                    <li className={style.cuore}> <span className='fw-bold'>Compleanno</span>: {villagerData['birthday']}</li>
-                                    <li className={style.cuore}> <span className='fw-bold'>Genere:</span> {genderTranslation(villagerData['gender'])} </li>
-                                    <li className={style.cuore}> <span className='fw-bold'>Personalità:</span> {personalitiesTranslation(villagerData['personality'])} </li>
-                                </ul>
-
-                            </div>
-
-                            <div className="col-2"> </div>
-
+                            </div> :
+                        <div>
+                        {
+                            apiError === false ?
+                                <div className={"text-center"}>
+                                    <h1>Sto per visualizzare il Villager #{villagerID}</h1>
+                                    <img src={require("../../assets/img/loading.gif")} alt="Loading icon" className="w-25"/>
+                                </div>
+                            :
+                            <h3 className='text-center'>Si è verificato un errore con il reperimento da API</h3>
+                        }
                         </div>
-
-                        <div className="row mt-3">
-                            <div className="col d-flex flex-column align-items-center">
-                                <div className={style.thought}> <p className="text-center"><span className='fw-bold'>Motto:</span> {villagerData['catch-translations']['catch-EUit']}</p>  </div>
-                                <p  className="text-center mt-4"><span className='fw-bold'>La sua citazione:</span> "{villagerData['saying']}"</p>
-                            </div>
-
-                        </div>
-
-                    </div> :
-                <div>
-                {
-                    apiError === false ?
-                        <div className={"text-center"}>
-                            <h1>Sto per visualizzare il Villager #{villagerID}</h1>
-                            <img src={require("../../assets/img/loading.gif")} alt="Loading icon" className="w-25"/>
-                        </div>
-                    :
-                    <h3 className='text-center'>Si è verificato un errore con il reperimento da API</h3>
-                }
-                </div>
-            }
-
-        </div>
+                    }
+                </>
+        }
+    </>
     )
 }
 
