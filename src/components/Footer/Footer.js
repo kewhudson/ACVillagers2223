ea4a508b9df015a7d630a3ab6b55c634c9104099
kewhudson <kewwoof@icloud.com>
2023-02-25 14:10:38 +0100

    Minor fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -28,48 +28,34 @@ function Footer(props){
         <div className="container-fluid">
 
             <div className="row">
-
                 <div className="col-md-5">
                     {<nav className={style.footerNav}>
-
                         <ul className="nav flex-column">
                             {itemList}
                         </ul>
-
                     </nav>}
-
-
                 </div>
 
-
                 <div className="col-md-7">
-
                     <div className={`d-flex ${style.copyright}`}>
-
-                        <div id={style.course}>
+                        <div>
                             <a href={courseLink} target="_blank">
                                 {courseName}
                             </a>
                         </div>
-
-                        <div id={style.disco} className={style.logo}>
+                        <div className={style.logo}>
                             <a href="https://www.disco.unimib.it/it" target="_blank">
                                 <img src={disco} alt="Logo DISCO"/>
                             </a>
                         </div>
-
-                        <div id={style.unimib} className={style.logo}>
+                        <div className={style.logo}>
                             <a href="https://www.unimib.it/" target="_blank">
                                 <img src={unimib} alt="Logo UNIMIB"/>
                             </a>
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
-
         </div>
         </footer>
     )
