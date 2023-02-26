2c203fb9ac71b3ef91e8a9af4b35ec4eafcbce95
kewhudson <kewwoof@icloud.com>
2023-02-26 22:37:51 +0100

    Minor fix

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Resetti from "../../assets/img/resetti-not-found.png";
-import DonResetti from "../../assets/img/don-resetti.png";
+import House from "../../assets/img/ac-house.png";
 import style from "./NotFound.module.css"
 import {NavLink} from "react-router-dom";
 
@@ -11,7 +11,7 @@ function NotFound() {
             <h1>Sei finito nella pagina 404</h1>
             <h2 className="mb-5">Quante volte ti ho detto di non uscire dalle rotte?!</h2>
             <NavLink className="button mt-3 mb-3 fw-bold" to="/">Torna alla Home</NavLink>
-            <NavLink to="/"><img src={DonResetti} alt="Don Resetti che si gratta la testa" className={style.imgDon}/></NavLink>
+            <NavLink to="/"><img src={House} alt="Casa di Animal Crossing" className={style.imgCasa}/></NavLink>
 
         </div>
     )
